4fb76a06234f0adef5415beb00f11316d15f8471
Mathis Chenuet <mathis.chenuet@unchartech.com>
2019-04-28 20:10:23 +0200

    oops named arguments

--- launch-network.sh
+++ launch-network.sh
@@ -5,7 +5,7 @@ mkfifo /tmp/launch.network.2
 
 cd $(dirname $0)
 
-uxterm -e 'sh -c "cargo run -- /tmp/launch.network.1 /tmp/launch.network.2"' &
-uxterm -e 'sh -c "cargo run -- /tmp/launch.network.2 /tmp/launch.network.1"' &
+uxterm -e 'sh -c "cargo run -- --input /tmp/launch.network.1 --output /tmp/launch.network.2"' &
+uxterm -e 'sh -c "cargo run -- --input /tmp/launch.network.2 --output /tmp/launch.network.1"' &
 
 wait
