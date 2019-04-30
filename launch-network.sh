cf54d730701251fef761dcd351626d465e5b4280
Mathis Chenuet <mathis.chenuet@unchartech.com>
2019-04-30 11:01:14 +0200

    use x-terminal-emulator

--- launch-network.sh
+++ launch-network.sh
@@ -5,7 +5,7 @@ mkfifo /tmp/launch.network.2
 
 cd $(dirname $0)
 
-uxterm -e 'sh -c "cargo run -- --input /tmp/launch.network.1 --output /tmp/launch.network.2"' &
-uxterm -e 'sh -c "cargo run -- --input /tmp/launch.network.2 --output /tmp/launch.network.1"' &
+x-terminal-emulator -e 'sh -c "cargo run -- --input /tmp/launch.network.1 --output /tmp/launch.network.2"' &
+x-terminal-emulator -e 'sh -c "cargo run -- --input /tmp/launch.network.2 --output /tmp/launch.network.1"' &
 
 wait
