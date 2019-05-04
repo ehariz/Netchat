47a80d0f8cb9a6b2b99c46401fc7235289d4fe38
Mathis Chenuet <mathis.chenuet@unchartech.com>
2019-05-05 00:37:56 +0200

    netcat demo

---
+++ netchat.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Script to test locally remote connections with netcat
+
+mkfifo in1 out1 in2 out2
+
+cat out1 | netcat -lv 1234 > in1 &
+cat out2 | netcat localhost 1234 > in2 &
+
+x-terminal-emulator -e 'cargo run -- -i in1 -o out1 -n ME' 
+x-terminal-emulator -e 'cargo run -- -i in2 -o out2 -n FRIEND'
+
+wait
